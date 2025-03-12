1859124701b5691f4d01190e34c09897a76ed7b0
CRochaOpt <carocha@nowoptics.com>
2025-03-12 17:07:15 -0600

    Adding better LSp and format support

--- lua/rocha/plugins/lsp.lua
+++ lua/rocha/plugins/lsp.lua
@@ -1,167 +1,180 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-        "onsails/lspkind.nvim"
-    },
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "j-hui/fidget.nvim",
+    "onsails/lspkind.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
+    "pmizio/typescript-tools.nvim",
+  },
 
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+  config = function()
+    local cmp = require('cmp')
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities())
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require('lspkind').init()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "gopls",
-                'tsserver',
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
+    require("fidget").setup({})
+    require("mason").setup()
+    require('lspkind').init()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "gopls",
+      },
+      handlers = {
+        function(server_name)         -- default handler (optional)
+          require("lspconfig")[server_name].setup {
+            capabilities = capabilities
+          }
+        end,
 
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-                ["html"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.html.setup({
-                        capabilities = capabilities,
-                        filetypes = { "html", "templ" },
-                    })
-                end,
-                ["htmx"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.htmx.setup({
-                        capabilities = capabilities,
-                        filetypes = { "html", "templ" },
-                    })
-                end,
-                ["tailwindcss"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.tailwindcss.setup({
-                        apabilities = capabilities,
-                        filetypes = { "templ", "astro", "javascript", "typescript", "react" },
-                        settings = {
-                            tailwindCSS = {
-                                includeLanguages = {
-                                    templ = "html",
-                                },
-                            },
-                        },
-                    })
-                end
-            }
-        })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-s>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            }),
-            window = {
-                completion = cmp.config.window.bordered(
-                    {
-                        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-                        col_offset = -3,
-                        side_padding = 0,
-                    }
-                ),
-
-                documentation = cmp.config.window.bordered(),
-            },
-            formatting = {
-                fields = { "kind", "abbr", "menu" },
-                format = function(entry, item)
-                    local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, item)
-                    local strings = vim.split(kind.kind, "%s", { trimempty = true })
-                    kind.kind = " " .. (strings[1] or "") .. " "
-                    kind.menu = "    (" .. (strings[2] or "") .. ")"
-                    return require("tailwindcss-colorizer-cmp").formatter(entry, kind)
-                end
-            }
-        })
-        cmp.setup.cmdline({ '/', '?' }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = 'buffer' },
-            }
-        })
-
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = 'path' }
-            }, {
-                {
-                    name = 'cmdline',
-                    option = {
-                        ingnore_cmds = { 'Man', '!' }
-                    }
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.lua_ls.setup {
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                runtime = { version = "Lua 5.1" },
+                diagnostics = {
+                  globals = { "vim", "it", "describe", "before_each", "after_each" },
                 }
-            }),
-            matching = { disallow_symbol_nonprefix_matching = false }
-        })
-
-        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
-
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = true,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
+              }
+            }
+          }
+        end,
+        ["html"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.html.setup({
+            capabilities = capabilities,
+            filetypes = { "html", "templ" },
+          })
+        end,
+        ["htmx"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.htmx.setup({
+            capabilities = capabilities,
+            filetypes = { "html", "templ" },
+          })
+        end,
+        ["tailwindcss"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.tailwindcss.setup({
+            apabilities = capabilities,
+            filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+            settings = {
+              tailwindCSS = {
+                includeLanguages = {
+                  templ = "html",
+                },
+              },
             },
-        })
-    end
+          })
+        end
+      }
+    })
+
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)           -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-s>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },         -- For luasnip users.
+      }, {
+        { name = 'buffer' },
+      }),
+      window = {
+        completion = cmp.config.window.bordered(
+          {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+          }
+        ),
+
+        documentation = cmp.config.window.bordered(),
+      },
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, item)
+          local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, item)
+          local strings = vim.split(kind.kind, "%s", { trimempty = true })
+          kind.kind = " " .. (strings[1] or "") .. " "
+          kind.menu = "    (" .. (strings[2] or "") .. ")"
+          return require("tailwindcss-colorizer-cmp").formatter(entry, kind)
+        end
+      }
+    })
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' },
+      }
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        {
+          name = 'cmdline',
+          option = {
+            ingnore_cmds = { 'Man', '!' }
+          }
+        }
+      }),
+      matching = { disallow_symbol_nonprefix_matching = false }
+    })
+
+    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = true,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+      },
+    })
+
+    require("typescript-tools").setup {}
+
+  end
 }
