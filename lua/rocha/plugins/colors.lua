abfb9d009720a5f66e88689d0e93915fdc8bb7b0
CRochaOpt <carocha@nowoptics.com>
2025-03-23 20:31:34 -0600

    Adding catpuccin support

--- lua/rocha/plugins/colors.lua
+++ lua/rocha/plugins/colors.lua
@@ -1,24 +1,36 @@
 function ColorNvim()
-	vim.api.nvim_set_hl(0,"Normal",{bg = "none"})
-	vim.api.nvim_set_hl(0,"NormalNC",{bg = "none"})
-	vim.api.nvim_set_hl(0,"NeoTreeNormal",{bg = "none"})
-	vim.api.nvim_set_hl(0,"NeoTreeNormalNC",{bg = "none"})
-	vim.api.nvim_set_hl(0,"NormalFloat",{bg = "none"})
-	vim.api.nvim_set_hl(0,"ColorColumn",{bg = "none"})
-
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
 end
 
 ColorNvim()
 
 return {
-    "catppuccin/nvim", name = "catppuccin", lazy = false , priority = 1000,
-    config = function ()
-        require("catppuccin").setup({
-            transparent_background = true, -- disables setting the background color.
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = true, -- disables setting the background color.
+      integrations = {
+        cmp = true,
+        snacks = {
+          enabled = true,
+        },
+        mini = {
+          enabled = true,
+        },
+        harpoon = true,
 
-        })
-        vim.cmd.colorscheme "tokyonight-storm"
-        ColorNvim()
-    end
+      }
+    })
+    vim.cmd.colorscheme "catppuccin"
+    ColorNvim()
+  end
 
 }
